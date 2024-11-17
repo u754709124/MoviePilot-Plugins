40578d0221cb5ed7d697bf568df69c310c2b8697
Chdon <u754709124@outlook.com>
2024-11-17 17:39:31 +0800

    测试

--- plugins/serverchan3msg/__init__.py
+++ plugins/serverchan3msg/__init__.py
@@ -16,7 +16,7 @@ class ServerChan3Msg(_PluginBase):
     # 插件图标
     plugin_icon = "ServerChan3.png"
     # 插件版本
-    plugin_version = "0.2"
+    plugin_version = "0.3"
     # 插件作者
     plugin_author = "Chdon"
     # 作者主页
