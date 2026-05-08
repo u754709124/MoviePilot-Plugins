5f29ffc99ab392de3fe882cf1ae6ebbd51ff2fa4
Chdon <chdonw@outlook.com>
2026-05-08 11:20:29 +0000

    fix: 插件版本统一

--- plugins.v2/oomsg/__init__.py
+++ plugins.v2/oomsg/__init__.py
@@ -15,7 +15,7 @@ class OoMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Wechat_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "chdon"
     # 作者主页
